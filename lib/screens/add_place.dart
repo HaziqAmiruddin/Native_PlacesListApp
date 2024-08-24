6a6a39437a5d049596e59f8995a9acf24afc5eb5
Haziq Amiruddin <141046553+HaziqAmiruddin@users.noreply.github.com>
2024-08-24 16:21:50 +0800

    Finish

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,15 +15,21 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final entredTitle = _titleController.text;
 
-    if (entredTitle.isEmpty) {
+    if (entredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(entredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(entredTitle, _selectedImage!, _selectedLocation!);
 
     Navigator.of(context).pop();
   }
@@ -45,6 +55,18 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               controller: _titleController,
               style:
                   TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(height: 16),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(height: 10),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
             ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
